73f8fe8dc650a148104cc5ef2c501fa7d64142ca
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-01-03 17:27:48 +0530

    React Elements

--- App.js
+++ App.js
@@ -17,12 +17,14 @@
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm H1 tag"),
-    React.createElement("h1", {}, "I'm H2 tag "),
+    React.createElement("h2", {}, "I'm H2 tag "),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm H1 tag"),
-    React.createElement("h1", {}, "I'm H2 tag "),
+    React.createElement("h2", {}, "I'm H2 tag "),
   ]),
 ]);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+console.log(parent);
