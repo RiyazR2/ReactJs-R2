f5ff23fd2799a1767dd7759fb70b8bdb6484591c
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-01-07 20:45:00 +0530

    JSX Started

--- App.js
+++ App.js
@@ -1,32 +1,7 @@
-/*
-
-<div id id="parent">
-  <div id="child">
-    <h1>I'm h1 tag</h1>
-    <h2>I'm h2 tag</h2>
-  </div>
-  <div id="child2">
-    <h1>I'm h1 tag</h1>
-    <h2>I'm h2 tag</h2>
-  </div>
-</div>
-
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm H1 tag"),
-    React.createElement("h2", {}, "I'm H2 tag "),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm H1 tag"),
-    React.createElement("h2", {}, "I'm H2 tag "),
-  ]),
-]);
-console.log(parent);
+const headingJSX = <h1>Hello World </h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-console.log(parent);
+root.render(headingJSX);
