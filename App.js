ffa9b1aeafb1637c669ffeab93abaafd931c645d
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-01-09 12:31:54 +0530

    Functional Components, Component Composition, JS inside JSX and many more

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
+// React Element
 const heading = React.createElement("h1", { id: "heading" }, "Hello World ");
 console.log(heading);
 
+// JSX React Element
 const headingJSX = <h1>Hello World using JSX </h1>;
-console.log(heading);
+console.log(headingJSX);
+
+// Functional Component
+const Author = () => <h1>Author: Riyaz</h1>;
+
+// React Functional Component
+const HeadingComponent = () => (
+  <>
+    {heading}
+    {headingJSX}
+    {Author()}
+    <Author />
+    <h1 className="heading">Nemaste React From Functional Component</h1>
+  </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(headingJSX);
+root.render(<HeadingComponent />);
