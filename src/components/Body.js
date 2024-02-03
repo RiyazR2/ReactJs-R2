960c96b59d4a03c6fcb8b1da37a5e818161e835b
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-02-03 20:39:06 +0530

    Started Code Exploring the world useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,21 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   let [ListOfRestaurants, setListOfRestaurants] = useState(resList);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+  };
   return (
     <div className="body">
       <div className="filter">
