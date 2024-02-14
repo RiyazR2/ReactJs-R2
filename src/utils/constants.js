3a26c1fc7780a73a23937f74a7f3d2ade560265d
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-02-14 07:42:40 +0530

    MENU_API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,14 @@ export const CDN_URL =
 
 export const LOGO_URL =
   "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+
+export const PROXY_CORS = "https://corsproxy.org/?";
+export const generateProxyUrl = (URL) => PROXY_CORS + encodeURIComponent(URL);
+
+export const swiggyAPI =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5642452&lng=73.7768511&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.6599188&lng=75.9063906&restaurantId=";
+// "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.6599188&lng=75.9063906&restaurantId"
+// 421568
