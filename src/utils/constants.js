6df8a2557f9da78e042cad8ed1064a38a4734f85
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-02-19 02:11:19 +0530

    added MENU_API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,7 +9,11 @@ export const PROXY_CORS = "https://corsproxy.org/?";
 export const generateProxyUrl = (URL) => PROXY_CORS + encodeURIComponent(URL);
 
 export const swiggyAPI =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5642452&lng=73.7768511&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.6599188&lng=75.9063906&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+// Location:- Pune Railway Station
+
+// "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.528913&lng=73.87441989999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+// "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5642452&lng=73.7768511&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.6599188&lng=75.9063906&restaurantId=";
