8a60e5525c47447d1b6bdf31fa0ea03213c02a4b
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-02-22 13:18:09 +0530

    Added Both Functional and Class Based Component in the About Section

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div>
-      <h1>About Us</h1>
-      <h2>This is Namaste React Web Series</h2>
+      {/* <h1>About Us</h1>
+      <h2>This is Namaste React Web Series</h2> */}
+
+      <User name="Riyaz" location="Solapur" />
+      <br />
+      <UserClass name="Riyaz" location="Solapur" />
     </div>
   );
 };
