8a87441691e3817e69f4e267f443aef7649ee9ee
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-02-24 22:10:14 +0530

    mount or loading,  how line by line flow

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,27 @@
-import User from "./User";
 import UserClass from "./UserClass";
+import { Component } from "react";
 
-const About = () => {
-  return (
-    <div>
-      {/* <h1>About Us</h1>
-      <h2>This is Namaste React Web Series</h2> */}
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Class Constructor");
+  }
 
-      <User name="Riyaz" location="Solapur" />
-      <br />
-      <UserClass name="Riyaz" location="Solapur" />
-    </div>
-  );
-};
+  componentDidMount() {
+    console.log("Parent Class Component Did Mount");
+  }
+  render() {
+    console.log("Parent Class Render");
+    return (
+      <div>
+        <h1>About Us</h1>
+        <h2>This is Namaste React Web Series</h2>
+
+        <br />
+        <UserClass name="Riyaz" location="Solapur" />
+      </div>
+    );
+  }
+}
 
 export default About;
