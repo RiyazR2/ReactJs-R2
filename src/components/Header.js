f5b94fb1c2d50bafbd9d34429025187d225920dc
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-02-29 12:14:37 +0530

    added custom hook  useOnlineStatus

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 export const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
@@ -8,6 +9,9 @@ export const Header = () => {
   useEffect(() => {
     console.log("useEffect Callled from Header Component");
   }, []);
+
+  const onlineStatus = useOnlineStatus();
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -15,6 +19,7 @@ export const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Status:{onlineStatus ? "🟢" : "🔴"}</li>
           <li>
             <Link to="/">Home</Link>
           </li>
