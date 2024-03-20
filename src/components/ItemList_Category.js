df01d0daa32860ae8cf0dc36c8f44028b230f0c3
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-03-20 11:56:48 +0530

    cart item list  info

--- src/components/ItemList_Category.js
+++ src/components/ItemList_Category.js
@@ -1,6 +1,14 @@
 import { CDN_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addItem } from "../utils/cartSlice";
 
 const ItemList_Category = ({ items }) => {
+  const dispatch = useDispatch();
+
+  const handleAddItem = (item) => {
+    // Dispatch an Action
+    dispatch(addItem(item));
+  };
   return (
     <div>
       {items.map((item) => (
@@ -23,12 +31,15 @@ const ItemList_Category = ({ items }) => {
           </div>
           <div className="w-3/12 p-4 ">
             <div className="absolute ml-5">
-              <button className=" w-24 p-1 font-semibold ml-3 mt-16  bg-white text-[#68ae52] border border-black rounded-lg  hover:bg-green-200">
+              <button
+                className=" w-24 p-1 font-semibold ml-3 mt-16  bg-white text-[#68ae52] border border-black rounded-lg  hover:bg-green-200"
+                onClick={() => handleAddItem(item)}
+              >
                 ADD
               </button>
             </div>
             <img
-              className="w-40 h-20 rounded-lg"
+              className="w-40 h-24 rounded-lg"
               src={CDN_URL + item?.card?.info?.imageId}
               alt="logo"
             />
