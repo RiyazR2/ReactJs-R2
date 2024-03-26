75bb5de035e5d25cf563ce4df571e8c8d1c9d596
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-03-26 11:15:59 +0530

    Test Cases for Header Component

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,68 @@
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import Header from "../Header";
+
+it("should render Header Component with Login Button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  //   const loginButton = screen.getByRole("button");
+  //   const loginButton = screen.getByText("Login");
+  const loginButton = screen.getByRole("button", { name: "Login" });
+
+  expect(loginButton).toBeInTheDocument();
+});
+
+it("should Login Button to Logout Button on Click", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", { name: "Login" });
+
+  fireEvent.click(loginButton);
+
+  const logoutButto = screen.getByRole("button", { name: "Logout" });
+
+  expect(logoutButto).toBeInTheDocument();
+});
+
+it("should render Header Component with Cart items 0", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const cartItem = screen.getByText("Cart - (0) 🛒");
+
+  expect(cartItem).toBeInTheDocument();
+});
+
+it("should render Header Component whith Cart Item", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const cartItem = screen.getByText(/Cart/);
+
+  expect(cartItem).toBeInTheDocument();
+});
