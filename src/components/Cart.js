4be8223226bbf5718a14ce1cde4439611a79ee39
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-04-04 12:36:18 +0530

    Added Total Amount option

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -15,6 +15,20 @@ const Cart = () => {
       <h1 className="text-2xl font-bold">Cart</h1>
       <div className="w-6/12 m-auto">
         <ItemList_Category items={cartItems} />
+        <div className="">
+          <button
+            className="p-2 m-2 font-bold ml-3 mt-16  bg-white text-black  border border-black rounded-lg  hover:bg-orange-500"
+            onClick={handleClearCart}
+          >
+            Clear Cart
+          </button>
+          <button className="p-2 m-2 font-bold ml-3 mt-16  bg-white text-black border border-black rounded-lg  hover:bg-green-400">
+            Total to Pay ={" "}
+            {cartItems
+              .map((it) => it.card.info.price / 100)
+              .reduce((acc, cur) => acc + cur, 0)}
+          </button>
+        </div>
       </div>
 
       {cartItems.length === 0 && (
@@ -22,12 +36,6 @@ const Cart = () => {
           Your cart is empty You can go to home page to view more restaurants
         </h1>
       )}
-      <button
-        className="p-2 m-2 bg-black text-white rounded-lg "
-        onClick={handleClearCart}
-      >
-        Clear Cart
-      </button>
     </div>
   );
 };
