9b6174784d48841d0220a0e68be03fd025db45c8
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-05-11 10:12:04 +0530

    Added one more user detail

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,8 +6,13 @@ class UserClass extends React.Component {
 
     this.state = {
       userInfo: {
-        name: "Dummy",
-        location: "Dummy",
+        name: "Initial_Dummy_Name",
+        location: "Initial_Dummy",
+        // avatar_url: "#Dummy",
+      },
+      userInfo2: {
+        name: "Initial_Dummy_Name",
+        location: "Initial_Dummy",
         // avatar_url: "#Dummy",
       },
     };
@@ -19,11 +24,16 @@ class UserClass extends React.Component {
     const data = await fetch("https://api.github.com/users/riyazr2");
     const json = await data.json();
 
+    const data2 = await fetch("https://api.github.com/users/RushikeshGandhmal");
+    const json2 = await data2.json();
+
     this.setState({
       userInfo: json,
+      userInfo2: json2,
     });
 
-    // console.log("userInfo: ",json);
+    console.log("userInfo: ", json);
+    console.log("userInfo2: ", json2);
   }
 
   render() {
@@ -45,10 +55,33 @@ class UserClass extends React.Component {
             className="font-semibold font-mono"
             style={{ whiteSpace: "pre-line" }}
           >
-            {name} {",  "}
-            riyazpathan193.rp@gmail.com
-            {"\n"} Solapur-Maharashtra {",   "}
+            {name}: {"\n"}Contact: riyazpathan193.rp@gmail.com{"\n"}
+            Solapur-Maharashtra {",   "}
             {location}
+          </h2>
+        </div>
+        <hr />
+        <hr />
+        <hr />
+        <hr />
+        <div className="flex justify-center items-center">
+          <img
+            className="avtar-logo mt-5 w-[160] h-[160] rounded-full"
+            src="https://avatars.githubusercontent.com/u/60548902?v=4"
+            alt=""
+          />
+        </div>
+        <div className=" my-5 ml-20 flex justify-center items-center ">
+          <h2
+            className="font-semibold font-mono"
+            style={{ whiteSpace: "pre-line" }}
+          >
+            {this.state.userInfo2.name}:{"\n"}
+            {this.state.userInfo2.bio}
+            Followers: {this.state.userInfo2.followers}
+            {"\n"}
+            Solapur-Maharashtra {",   "}
+            {this.state.userInfo2.location}
           </h2>
         </div>
       </div>
