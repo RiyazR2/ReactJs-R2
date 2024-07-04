5eb48cc5ff3930117fb2d9cb1459ac00d2457210
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-07-04 22:57:30 +0530

    Updates

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -12,7 +12,9 @@ const useRestaurantMenu = (resId) => {
 
   const fetchData = async () => {
     const data = await fetch(MENU_API + resId);
+
     const json = await data.json();
+    console.log("MenuAPI:", json);
     setResInfo(json.data);
   };
 
