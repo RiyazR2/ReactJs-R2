fb937ac247c2383ad85a26960c7f0383a0a6f2b2
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-07-05 16:32:52 +0530

    Updated

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,37 +1,37 @@
 import { CDN_URL } from "../utils/constants";
-// import UserContext from "../utils/UserContext";
-// import { useContext } from "react";
+
 const RestaurantCard = (props) => {
   const { restData } = props;
-  // console.log(restData);
+
   const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
     restData?.info;
 
-  // const { loggedInUser } = useContext(UserContext);
   return (
     <div
       data-testid="resCard"
-      className="relative mx-8 p-4 w-[250px] rounded-lg bg-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out hover:-translate-y-2"
+      className="relative flex flex-col justify-between mx-6 mb-10 p-4 w-[250px] h-[400px] rounded-lg bg-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out hover:-translate-y-2 overflow-hidden"
     >
       <div className="relative overflow-hidden rounded-lg">
         <img
           className="w-full h-[200px] object-cover rounded-lg transition-transform duration-300 ease-in-out transform hover:scale-110"
-          //           alt="res-logo"
           alt="res-logo"
           src={CDN_URL + cloudinaryImageId}
         />
       </div>
 
-      <h3 className="font-bold py-3 text-lg text-gray-800">{name}</h3>
-      <h4 className="font-semibold py-1 text-gray-600">
-        {cuisines.join(", ")}
-      </h4>
-      <div className="flex justify-between items-center py-2 text-gray-600">
-        <span className="font-semibold">{avgRating} stars</span>
-        <span className="font-semibold">{costForTwo}</span>
+      <div className="flex flex-col justify-between flex-grow mt-3">
+        <div>
+          <h3 className="font-bold text-lg text-gray-800">{name}</h3>
+          <h4 className="font-semibold text-gray-600">{cuisines.join(", ")}</h4>
+        </div>
+
+        <div className="flex justify-between items-center text-gray-600 mt-2">
+          <span className="font-semibold">{avgRating} stars</span>
+          <span className="font-semibold">{costForTwo}</span>
+        </div>
+
+        <h4 className="font-semibold text-gray-600">{sla?.slaString}</h4>
       </div>
-      <h4 className="font-semibold py-1 text-gray-600">{sla?.slaString}</h4>
-      {/* sla?.slaString === Delivery Time */}
     </div>
   );
 };
@@ -42,7 +42,6 @@ const RestaurantCard = (props) => {
 export const withDiscountLabel = (RestaurantCard) => {
   return (props) => {
     const { header } = props?.restData?.info?.aggregatedDiscountInfoV3;
-    // console.log("header", props?.restData?.info?.aggregatedDiscountInfoV3);
     return (
       <div className="relative">
         <label className="absolute bg-black text-white rounded-md ml-8 p-2 z-10">
@@ -55,35 +54,32 @@ export const withDiscountLabel = (RestaurantCard) => {
 };
 export default RestaurantCard;
 
-/* ****************************** Updated Tailwindcss Style ****************************** */
+/* ****************************** 👆 Updated Tailwindcss Style ****************************** */
 
 // import { CDN_URL } from "../utils/constants";
-// import UserContext from "../utils/UserContext";
-// import { useContext } from "react";
-
+// // import UserContext from "../utils/UserContext";
+// // import { useContext } from "react";
 // const RestaurantCard = (props) => {
 //   const { restData } = props;
+//   // console.log(restData);
 //   const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
 //     restData?.info;
-//   const { loggedInUser } = useContext(UserContext);
 
+//   // const { loggedInUser } = useContext(UserContext);
 //   return (
 //     <div
 //       data-testid="resCard"
-//       className="m-4 p-6 w-[300px] rounded-lg bg-white shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out transform hover:-translate-y-2"
+//       className="relative  ml-8 p-4 w-[250px] rounded-lg bg-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out hover:-translate-y-2"
 //     >
 //       <div className="relative overflow-hidden rounded-lg">
 //         <img
 //           className="w-full h-[200px] object-cover rounded-lg transition-transform duration-300 ease-in-out transform hover:scale-110"
+//           //           alt="res-logo"
 //           alt="res-logo"
 //           src={CDN_URL + cloudinaryImageId}
 //         />
-//         {props?.restData?.info?.aggregatedDiscountInfoV3?.header && (
-//           <div className="absolute top-2 left-2 bg-black text-white text-xs px-2 py-1 rounded">
-//             {props.restData.info.aggregatedDiscountInfoV3.header}
-//           </div>
-//         )}
 //       </div>
+
 //       <h3 className="font-bold py-3 text-lg text-gray-800">{name}</h3>
 //       <h4 className="font-semibold py-1 text-gray-600">
 //         {cuisines.join(", ")}
@@ -93,6 +89,7 @@ export default RestaurantCard;
 //         <span className="font-semibold">{costForTwo}</span>
 //       </div>
 //       <h4 className="font-semibold py-1 text-gray-600">{sla?.slaString}</h4>
+//       {/* sla?.slaString === Delivery Time */}
 //     </div>
 //   );
 // };
@@ -105,14 +102,13 @@ export default RestaurantCard;
 //     const { header } = props?.restData?.info?.aggregatedDiscountInfoV3;
 //     // console.log("header", props?.restData?.info?.aggregatedDiscountInfoV3);
 //     return (
-//       <div>
-//         <label className="absolute bg-black text-white rounded-md ml-4 p-2 ">
-//           {!header.includes("ITEMS") ? "Discount Soon 🤗" : header}
+//       <div className="relative">
+//         <label className="absolute bg-black text-white rounded-md ml-8 p-2 z-10">
+//           {!header.includes("OFF") ? "Discount Soon 🤗" : header}
 //         </label>
 //         <RestaurantCard {...props} />
 //       </div>
 //     );
 //   };
 // };
-
 // export default RestaurantCard;
