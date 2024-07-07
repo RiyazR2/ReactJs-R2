480ec8ccb4d61f834a1cd0df64ae2c7ffe138238
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-07-07 23:30:22 +0530

    Styled Updated

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,42 +1,120 @@
 import { useDispatch, useSelector } from "react-redux";
 import ItemList_Category from "./ItemList_Category";
 import { clearCart } from "../utils/cartSlice";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
-
   const dispatch = useDispatch();
 
   const handleClearCart = () => {
     dispatch(clearCart());
   };
-  return (
-    <div className="text-center m-4 p-4  ">
-      <h1 className="text-2xl font-bold">Cart</h1>
-      <div className="w-6/12 m-auto">
-        <ItemList_Category items={cartItems} />
-        <div className="">
-          <button
-            className="p-2 m-2 font-bold ml-3 mt-16  bg-white text-black  border border-black rounded-lg  hover:bg-orange-500"
-            onClick={handleClearCart}
-          >
-            Clear Cart
-          </button>
-          <button className="p-2 m-2 font-bold ml-3 mt-16  bg-white text-black border border-black rounded-lg  hover:bg-green-400">
-            Total to Pay ={" "}
-            {cartItems
-              .map((it) => it.card.info.price / 100)
-              .reduce((acc, cur) => acc + cur, 0)}
-          </button>
-        </div>
-      </div>
 
-      {cartItems.length === 0 && (
-        <h1>
-          Your cart is empty You can go to home page to view more restaurants
-        </h1>
-      )}
+  const totalToPay = cartItems
+    .map(
+      (item) => item.card.info.price / 100 || item.card.info.defaultPrice / 100
+    )
+    .reduce((acc, cur) => acc + cur, 0);
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-4 flex flex-col items-center">
+      <h1 className="text-3xl font-bold text-gray-800 mb-6">Your Cart</h1>
+      <div className="w-full max-w-4xl bg-white rounded-lg shadow-md p-6">
+        {cartItems.length === 0 ? (
+          <div className="text-center">
+            <h2 className="text-xl font-semibold text-gray-700">
+              Your cart is empty.
+            </h2>
+            <p className="text-gray-600">
+              You can go to the home page to view more restaurants.
+            </p>
+            <Link
+              to="/"
+              className="mt-4 inline-block bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600 transition duration-300"
+            >
+              Go to Home
+            </Link>
+          </div>
+        ) : (
+          <>
+            <ItemList_Category items={cartItems} />
+            <div className="flex justify-between items-center mt-6">
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded-lg shadow hover:bg-red-600 transition duration-300"
+                onClick={handleClearCart}
+              >
+                Clear Cart
+              </button>
+              <Link to="/payment">
+                <button
+                  className="bg-green-500 text-white px-4 py-2 rounded-lg shadow hover:bg-green-600 transition duration-300"
+                  onClick={handleClearCart}
+                >
+                  Total to Pay: ₹{totalToPay}
+                </button>
+              </Link>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
+
 export default Cart;
+
+// import { useDispatch, useSelector } from "react-redux";
+// import ItemList_Category from "./ItemList_Category";
+// import { clearCart } from "../utils/cartSlice";
+// import { Link } from "react-router-dom";
+
+// const Cart = () => {
+//   const cartItems = useSelector((store) => store.cart.items);
+
+//   const dispatch = useDispatch();
+
+//   const handleClearCart = () => {
+//     dispatch(clearCart());
+//   };
+//   return (
+//     <div className="text-center m-4 p-4  ">
+//       <h1 className="text-2xl font-bold">Cart</h1>
+//       <div className="w-6/12 m-auto">
+//         <ItemList_Category items={cartItems} />
+//         <div className="">
+//           <Link to="/clear">
+//             <button
+//               className="p-2 m-2 font-bold ml-3 mt-16  bg-white text-black  border border-black rounded-lg  hover:bg-orange-500"
+//               onClick={handleClearCart}
+//             >
+//               Clear Cart
+//             </button>
+//           </Link>
+//           <Link to="/payment">
+//             <button
+//               className="p-2 m-2 font-bold ml-3 mt-16  bg-white text-black border border-black rounded-lg  hover:bg-green-400"
+//               onClick={handleClearCart}
+//             >
+//               Total to Pay ={" "}
+//               {cartItems
+//                 .map(
+//                   (it) =>
+//                     it.card.info.price / 100 || it.card.info.defaultPrice / 100
+//                 )
+//                 .reduce((acc, cur) => acc + cur, 0)}
+//               {console.log("Button ", cartItems)}
+//             </button>
+//           </Link>
+//         </div>
+//       </div>
+
+//       {cartItems.length === 0 && (
+//         <h1>
+//           Your cart is empty You can go to home page to view more restaurants
+//         </h1>
+//       )}
+//     </div>
+//   );
+// };
+// export default Cart;
