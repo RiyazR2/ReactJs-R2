31dba827478043fe89229c343a26945b21e0c280
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-07-11 09:08:29 +0530

    Minor Updates

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -19,9 +19,6 @@ const RestaurantMenu = () => {
   const { name, cuisines, costForTwoMessage, avgRating } =
     restaurantInfo?.cards[2]?.card?.card?.info; // ! Always check the path [json / restaurantInfo]
 
-  // const { itemCards } =
-  //   restaurantInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card; // Always check the path [json / restaurantInfo]
-  // console.log("itemCards",restaurantInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
   const categories =
     restaurantInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (c) =>
@@ -47,20 +44,6 @@ const RestaurantMenu = () => {
           setShowIndex={() => setShowIndex(index)} // Passing setShowIndex to my Children, I will pass a function and this function basically setShowIndex of that particular index
         />
       ))}
-
-      {/* <hr />
-
-      <h2 className="font-bold mt-5 ml-10 text-black">MENU</h2>
-      <ul className="ml-10">
-        {itemCards?.map((item) => (
-          <li key={item?.card?.info?.id}>
-            {item?.card?.info?.name} - {" Rs."}
-            {item?.card?.info?.price / 100 ||
-              item?.card?.info?.defaultPrice / 100}
-          </li>
-        ))}
-        <h1 align="center">More Info Will be Added Soon... 🤗</h1>
-      </ul> */}
     </div>
   );
 };
